39a944fb3c22cbc039028de6fc7f9bccb1ca8811
AnastasiyaSavelyeva <savelas22@yandex.ru>
2023-02-16 11:18:47 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow: '.arrow-prev',
+        nextArrow: '.arrow-next',
+        dots: true,
+        infinite: true,
+        variableWidth: true,
+        arrows: true,
+        dotsClass: 'slick-dots',
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+
+
+
+
+
+
+
+});
