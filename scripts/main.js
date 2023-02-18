7db1c53efe25dc6716e8da8500dc2f06204cd641
AnastasiyaSavelyeva <savelas22@yandex.ru>
2023-02-18 21:30:32 +0300

    form validation

--- scripts/main.js
+++ scripts/main.js
@@ -69,16 +69,27 @@ $(document).ready(function(){
         type: 'image'
     });
 
-   //form1
+   //form
 
-    const name = $('#name');
-    const phone = $('#phone');
-    let check = $('input[type=checkbox]');
-    let errorCheck = $('.error-check');
+    let loader = $('#loader');
 
-    phone.inputmask({"mask": "(999) 999-9999"});
 
-    $('#getConsultation').click((e)=> {
+
+    $('#getConsultation, #getConsultation2').click((e)=> {
+
+        let name = $('#name');
+        let name2 = $('#name2');
+        let phone = $('#phone');
+        let phone2 = $('#phone2');
+        let form1 = $('#form1');
+        let form2 = $('#form2');
+        let thanks = $('.thanks');
+        let check = $('input[type=checkbox]');
+        let errorCheck = $('.error-check');
+        let hasError = false;
+
+        phone2.inputmask({"mask": "(999) 999-9999"});
+        phone.inputmask({"mask": "(999) 999-9999"});
 
         e.preventDefault();
 
@@ -86,53 +97,70 @@ $(document).ready(function(){
         errorCheck.hide();
         $('input').css('border-color', 'white');
 
+
        if(!name.val()) {
            name.next().show();
            name.css('border-color', '#ad0707');
+           hasError = true;
        }
+       /*if(!name2.val()) {
+           name2.next().show();
+           name2.css('border-color', '#ad0707');
+           hasError = true;
+       }*/
+
        if(!phone.val()) {
            phone.next().show();
            phone.css('border-color', '#ad0707');
+           hasError = true;
        }
+
+       /*if(!phone2.val()) {
+           phone2.next().show();
+           phone2.css('border-color', '#ad0707');
+           hasError = true;
+       }*/
 
         if(!check.is(':checked')) {
            errorCheck.show();
-           $('#agreement').hide();
+           $('.agreement').hide();
+            hasError = true;
+        }
+
+        if(!hasError) {
+            loader.css('display', 'flex')
+            form1.hide();
+            $('#consultation-title').hide();
+
+           /* form2.hide();*/
+
+            $.ajax({
+                method: "POST",
+                url: "https://testologia.site/checkout",
+                data: {name: name.val(), phone: phone.val(), check: check.checked}
+            })
+                .done(function (msg) {
+                    loader.hide()
+                    if (msg.success) {
+                        thanks.css('display', 'flex');
+
+                    } else {
+                        $('#consultation-title').show();
+                        form1.trigger("reset");
+                        form1.show();
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Возникла ошибка при оформлении заказа',
+                            text: 'Попробуйте снова ввести данные или перезвоните нам',
+                            confirmButtonColor: '#ecc66b',
+
+                        })
+                    }
+                });
         }
     })
-   //form2
-
-    const name2 = $('#name2');
-    const phone2 = $('#phone2');
-    let check2 = $('.popup input[type=checkbox]');
-    console.log(check2)
-    let errorCheck2 = $('.popup .error-check');
-
-    phone2.inputmask({"mask": "(999) 999-9999"});
 
 
-       $('#getConsultation2').click((event)=> {
-
-        event.preventDefault();
-
-        $('.error').hide();
-        errorCheck2.hide();
-        $('input').css('border-color', 'white');
-
-       if(!name2.val()) {
-           name2.next().show();
-           name2.css('border-color', '#ad0707');
-       }
-       if(!phone2.val()) {
-           phone2.next().show();
-           phone2.css('border-color', '#ad0707');
-       }
-
-        if(!check2.is(':checked')) {
-           errorCheck2.show();
-            $('#agreement2').hide();
-        }
-    })
 
 
 
